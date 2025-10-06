ae8503c88d1d757afbd9e8c725fc094dd74d3bea
rjaymf10 <32949974+rjaymf10@users.noreply.github.com>
2025-10-06 14:30:35 +0800

    Update network.ts

--- src/configs/network.ts
+++ src/configs/network.ts
@@ -1,3 +1,3 @@
-export const CONTRACTPACKAGEID = "0xca3e549c7e1e56ec39c16b92560c08b41b308e07408fa00d6c51c561f996b097";
-export const CONTRACTMODULENAME = "testnet_nft";
-export const CONTRACTMODULEMETHOD = "mint_to_sender";
+export const CONTRACTPACKAGEID = "";
+export const CONTRACTMODULENAME = "";
+export const CONTRACTMODULEMETHOD = "";
